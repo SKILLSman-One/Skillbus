5cc9f4b54f912a61f432c519a4d44527f25b8128
SKILLSman-One <61345698+SKILLSman-One@users.noreply.github.com>
2022-08-30 03:13:57 +0500

    Delete Skillbus directory

--- Skillbus/crm-frontend/js/const.js
+++
@@ -1,7 +0,0 @@
-import {createHeader, createMain, createButtonAddClient, createTableLoading} from './elements.js';
-
-export const BODY = document.querySelector('body'),
-              HEADER = createHeader(),
-              MAIN = createMain(),
-              LOADING = createTableLoading(),
-              BTN_ADD_CLIENT = createButtonAddClient()
